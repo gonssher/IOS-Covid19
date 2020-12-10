2ba7103682e3a7587a2f3243e14db26bbaedb4fb
Sherwin Gonsalves <52938571+gonssher@users.noreply.github.com>
2020-12-10 04:22:47 +0300

    //Commit updated changes to ui
    This is almost complete very close to completion and submission a bit of work left to do .

--- Assignment2_SherwinGonsalves_991489757/ChartScene.swift
+++ Assignment2_SherwinGonsalves_991489757/ChartScene.swift
@@ -7,7 +7,64 @@
 
 import UIKit
 import SpriteKit
+import GameplayKit
+
 
 class ChartScene: SKScene {
 
-}
+    var shapeLine1 = SKShapeNode()
+    let circle = SKShapeNode(circleOfRadius: 15)
+ 
+    func updateChart(_ values: [Int]) {
+        
+         let path1 = CGMutablePath()
+        
+        let scaleX = 800 /  CGFloat(values.count - 1)
+        let maxValue = values.max() ?? 0
+        let scaleY = 800 / CGFloat(maxValue)
+        let point = CGPoint(x: 12 * scaleX + 150, y: 34 * scaleY + 150)
+        
+        for i in 0 ..< values.count-1
+        {
+            
+           path1.move(to: CGPoint(x: scaleX * CGFloat(i) + 100,y: scaleY * CGFloat(values[i]) + 100))
+           path1.addLine(to: CGPoint(x: scaleX * CGFloat(i+1) + 100,y: scaleY * CGFloat(values[i+1]) + 100))
+       
+            circle.position = CGPoint(x: scaleX * CGFloat(i+1) + 100,y: scaleY * CGFloat(values[i+1]) + 100)
+       
+            //circle.Position = CGPoint(x: Int(scaleX) * CGFloat(i+1) + 100,y: Int(scaleY) * CGFloat(values[i+1]) + 100)
+        }
+
+        shapeLine1.removeFromParent()
+         shapeLine1.path = path1
+         shapeLine1.strokeColor = .systemBlue
+         shapeLine1.lineWidth = 9
+        
+        circle.removeFromParent()
+        circle.fillColor = .white // fill colour
+        circle.strokeColor = .systemRed // border colour
+        circle.lineWidth = 10 // border width
+       circle.zPosition = 2 // z-index
+       // circle.position.x =   path1.move(to: CGPoint(x: scaleX * CGFloat(i) + 100,y: scaleY * CGFloat(values[i]) + 100)) // position
+        
+        self.addChild(circle)
+         self.addChild(shapeLine1)
+
+
+        
+       
+     
+    }
+
+    
+    
+    
+ 
+   
+    
+ }
+    
+    
+
+    
+    
--- Assignment2_SherwinGonsalves_991489757/ViewController.swift
+++ Assignment2_SherwinGonsalves_991489757/ViewController.swift
@@ -6,22 +6,34 @@
 //
 
 import UIKit
+import SpriteKit
+import GameplayKit
+
+
 
 class ViewController: UIViewController,UIPickerViewDataSource, UIPickerViewDelegate {
     
-   
-    var covidDate = [Covid]()
+
     @IBOutlet weak var pickerView : UIPickerView!
     @IBOutlet weak var labelToday : UILabel!
     @IBOutlet weak var labelTotal : UILabel!
+    @IBOutlet weak var skView: SKView!
+    var scene: ChartScene!
+    
+    
     var dates = [String]()
     var dateSet: Set<String> = [] // create a temp set
     var passedIndexvalue : Int = -1
+    var values = [Int]()
+    var jsonPassData = [[String:Any]]()
+
+    
     @IBOutlet weak var provinceselction: UISegmentedControl!
     var provincePass : String = ""
     var passDate : String = ""
-    
+    let selectedINdex = 0
     override func viewDidLoad() {
+     
         super.viewDidLoad()
         requestJson()
         pickerView.delegate = self
@@ -31,8 +43,22 @@ class ViewController: UIViewController,UIPickerViewDataSource, UIPickerViewDeleg
         {
            self.pickerView.reloadAllComponents()
         }
+        
+        provinceselction.selectedSegmentIndex = selectedINdex
+        if let scene = SKScene(fileNamed: "ChartScene")
+        {
+            scene.scaleMode = .aspectFit
+            skView.presentScene(scene)
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+
+        }
+
+        
     }
 
+
+    
     @IBAction func changeProvince(_ sender: Any) {
         
         if provinceselction.selectedSegmentIndex == 0
@@ -40,26 +66,27 @@ class ViewController: UIViewController,UIPickerViewDataSource, UIPickerViewDeleg
             self.pickerView.reloadComponent(0)
             self.pickerView.selectRow(self.dates.count-1,inComponent: 0, animated: false)
             passedIndexvalue = dates.count-1
+            passDate = String(dates.last!)
             labelToday.text = "N/A"
             labelTotal.text = "N/A"
             let province = "Canada"
             provincePass = province
-            requestJson()
-         
+            print(passDate)
+            self.passProvinceData(_Country: provincePass)
            print("Loading Cases For Picker Index Value 0 \(provincePass)")
             
         }
         else if provinceselction.selectedSegmentIndex == 1
         {
             self.pickerView.reloadComponent(0)
-            self.pickerView.selectRow(self.dates.count-1 ,inComponent: 0, animated: false)
+            self.pickerView.selectRow(self.dates.count-1,inComponent: 0, animated: false)
             passedIndexvalue = dates.count-1
+            passDate = String(dates.last!)
             labelToday.text = "N/A"
             labelTotal.text = "N/A"
             let province = "Ontario"
             provincePass = province
-            requestJson()
-            
+            self.passProvinceData(_Country: provincePass)
            print("Loading Cases For Picker Index Value 1 \(provincePass)")
         }
         else if provinceselction.selectedSegmentIndex == 2
@@ -67,11 +94,14 @@ class ViewController: UIViewController,UIPickerViewDataSource, UIPickerViewDeleg
             self.pickerView.reloadComponent(0)
             self.pickerView.selectRow(self.dates.count-1 ,inComponent: 0, animated: false)
             passedIndexvalue = dates.count-1
+            passDate = String(dates.last!)
             labelToday.text = "N/A"
             labelTotal.text = "N/A"
             let province = "Quebec"
             provincePass = province
-            requestJson()
+            print(passDate)
+            self.passProvinceData(_Country: provincePass)
+
            print("Loading Cases For Picker Index Value 2 \(provincePass)")
         }
         else if provinceselction.selectedSegmentIndex == 3
@@ -80,18 +110,20 @@ class ViewController: UIViewController,UIPickerViewDataSource, UIPickerViewDeleg
             self.pickerView.reloadComponent(0)
             self.pickerView.selectRow(self.dates.count-1 ,inComponent: 0, animated: false)
             passedIndexvalue = dates.count-1
+            passDate = String(dates.last!)
             labelToday.text = "N/A"
             labelTotal.text = "N/A"
             let province = "British Columbia"
             provincePass = province
-            requestJson()
+            print(passDate)
+            self.passProvinceData(_Country: provincePass)
            print("Loading Cases For Picker Index Value 3 \(provincePass)")
 
         }
  
     }
     
-    
+
     func requestJson()
     {
         let url = URL(string:"http://ejd.songho.ca/ios/covid19.json")
@@ -106,7 +138,9 @@ class ViewController: UIViewController,UIPickerViewDataSource, UIPickerViewDeleg
             }
             
         }.resume()
+     
     }
+    
 
     
     func parseJson(_ data: Data)
@@ -116,85 +150,64 @@ class ViewController: UIViewController,UIPickerViewDataSource, UIPickerViewDeleg
      if let json = try JSONSerialization.jsonObject(with:data,
      options:[]) as? [[String:Any]]
      {
+        self.jsonPassData = json
+        
         for dict in json// json is array of dictionaries
         {
             if let date = dict["date"] as? String {
              dateSet.insert(date)
              }
             self.dates = dateSet.sorted()
-            
+
         }
-        
-    if passedIndexvalue == -1
+  
+        if passedIndexvalue == -1
         {
             DispatchQueue.main.async
             {
            self.labelToday.text = "N/A"
            self.labelTotal.text = "N/A"
+                self.pickerView.reloadComponent(0)
+                self.pickerView.selectRow(self.dates.count-1 ,inComponent: 0, animated: false)
+                
             }
-        print("No Data Available for this index value as yet \(dates.count-1)")
-        }
+            let prov = provincePass
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd"
         
- 
-        else
+            let SecPerDay: Double = 60 * 60 * 24 // seconds a day
+            let firstDate = formatter.date(from: dates.first!) ?? Date()
+            let lastDate = formatter.date(from: dates.last!) ?? Date()
+            let sec = lastDate.timeIntervalSince(firstDate)
+            let dayCount = Int(sec / SecPerDay + 0.5) + 1
+            self.values = [Int](repeating: 0, count: dayCount)
+            for i in 0 ..< values.count
+                {
+                let date = firstDate.addingTimeInterval(Double(i) * SecPerDay)
+                let dateStrs = formatter.string(from: date)
+                    
+                if let index = json.firstIndex(where: { $0["prname"] as? String == prov && $0["date"] as? String == dateStrs })
+                {
+                    values[i] = json[index]["numtoday"] as? Int ?? 0
+                }
+                    
+                }
+                print(values)
+            print("No Data Available as yet")
+        if let scene = skView.scene as? ChartScene
         {
+            scene.updateChart(values)
+        }
 
-        let prov = provincePass
-        let dateStr = self.pickerView(pickerView, titleForRow: passedIndexvalue, forComponent: 0) ?? ""
-    
-        if let i = json.firstIndex(where: { $0["prname"] as? String == prov && $0["date"] as? String == dateStr })
-        {
-            DispatchQueue.main.async
-            {
-            
-                self.labelToday.text =  ("Daily \n \(String(json[i]["numtoday"] as? Int ?? 0)) ")
-                self.labelTotal.text = ("Total \n \(String(json[i]["numtotal"] as? Int ?? 0)) ")
-              
-                //Testing Data
-                print((" ************************** Province Data for \(String((json[i]["prname"] as? String)!)) ************************"))
-                print("Passed index value from picker view is \(self.passedIndexvalue)");
-                print("Date Selected is \(self.passDate)")
-                print( "Daily Cases are \(String(json[i]["numtoday"] as? Int ?? 0)) ")
-                print("Total Cases are \(String(json[i]["numtotal"] as? Int ?? 0)) ")
-                print((" *********************************** The End **********************************"))
-            }
-          
+        }
         
-            
-        }
-        else {
-            
-            DispatchQueue.main.async
-            {
-                self.labelToday.text = "N/A"
-                self.labelTotal.text = "N/A"
-                print("No Data Available for this date \(self.passDate)")
-            }
-        }
-   
-     }
      }
      
-     else
-     {
-        return
-     }
-     // draw chart first time after parsing JSON
-        DispatchQueue.main.async
-        {
-            if self.passedIndexvalue == -1
-        {
-            
-        self.pickerView.reloadComponent(0)
-        self.pickerView.selectRow(self.dates.count-1 ,inComponent: 0, animated: false)
-        }
-        
-        }
         }
         catch
         {
-
-     return
+            showAlert(message: "Erro Parsing Data")
+            return
         }
     }
     
@@ -231,14 +244,94 @@ class ViewController: UIViewController,UIPickerViewDataSource, UIPickerViewDeleg
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
     
         let selectedDate = dates[row]
-        //print("Selected Date  " + selectedDate);
         passDate = selectedDate
         let index = pickerView.selectedRow(inComponent: 0)
-     //   print("Selected Index Value \(index)");
         passedIndexvalue = index
-        requestJson()
+        self.passProvinceData(_Country: provincePass)
+        
+        
+  /*      ********* TESTiING USER DATA **********
+        print("Selected Index Value \(index)");
+        print("Selected Date  " + selectedDate);
+ */
+        
+ }
+ 
     
+    
+    
+    func passProvinceData(_Country : String)
+    
+    {
+        let prov = provincePass
+        let dateStr = self.pickerView(pickerView, titleForRow: passedIndexvalue, forComponent: 0) ?? ""
+        
+       // let prov = provincePass
+       // let dateStr = self.pickerView(pickerView, titleForRow: passedIndexvalue, forComponent: 0) ?? ""
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd"
+           // let dateString = "2020-01-31"
+            let SecPerDay: Double = 60 * 60 * 24 // seconds a day
+            let firstDate = formatter.date(from: dates.first!) ?? Date()
+            let lastDate = formatter.date(from: passDate) ?? Date()
+            let sec = lastDate.timeIntervalSince(firstDate)
+            let dayCount = Int(sec / SecPerDay + 0.5) + 1
+
+            self.values = [Int](repeating: 0, count: dayCount)
+            for i in 0 ..< values.count
+                {
+                
+                let date = firstDate.addingTimeInterval(Double(i) * SecPerDay)
+                let dateStrs = formatter.string(from: date)
+                    
+                if let index = jsonPassData.firstIndex(where: { $0["prname"] as? String == prov && $0["date"] as? String == dateStrs })
+                {
+                    // found date, put the value from JSON
+                    values[i] = jsonPassData[index]["numtoday"] as? Int ?? 0
+                }
+                    
+                }
+            if let scene = skView.scene as? ChartScene
+            {
+            
+                scene.updateChart(values)
+                
+            }
+        
+        if let i = jsonPassData.firstIndex(where: { $0["prname"] as? String == prov && $0["date"] as? String == dateStr })
+        {
+      
+            DispatchQueue.main.async
+            {
+            
+                self.labelToday.text =  ("Daily \n \(String(self.jsonPassData[i]["numtoday"] as? Int ?? 0)) ")
+                self.labelTotal.text = ("Total \n \(String(self.jsonPassData[i]["numtotal"] as? Int ?? 0)) ")
+                
+                /*      ********* TESTiING USER DATA **********
+                print((" ************************** Province Data for \(String((self.jsonPassData[i]["prname"] as? String)!)) ************************"))
+                print("Passed index value from picker view is \(self.passedIndexvalue)");
+                print("Date Selected is \(self.passDate)")
+                print( "Daily Cases are \(String(self.jsonPassData[i]["numtoday"] as? Int ?? 0)) ")
+                print("Total Cases are \(String(self.jsonPassData[i]["numtotal"] as? Int ?? 0)) ")
+                print((" *********************************** The End **********************************"))
+                 */
+            }
+            
+         
+        }
+        
+        else {
+            DispatchQueue.main.async
+            {
+                self.labelToday.text = "N/A"
+                self.labelTotal.text = "N/A"
+                print("No Data Available for this date ")
+            }
+           
+        }
+        
     }
+    
   
 }
 
